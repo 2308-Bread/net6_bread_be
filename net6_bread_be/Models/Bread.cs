3a25b89bf7ca999d5cff1250f76b5f4dcf6ccd26
Eric Belongea <eric.belongea@gmail.com>
2024-02-24 17:00:35 -0700

    Bread model and testing
    This will create the bread model and a test. The test does not pass yet

---
+++ net6_bread_be/Models/Bread.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace net6_bread_be.Models
+{
+	public class Bread
+	{
+		public int BreadId { get; set;}
+		public string Name { get; set; } = "";
+		public string Recipe { get; set; } = "";
+		public string Description { get; set; } = "";
+	}
+}
+
---
+++ net6_bread_be_tests/Models/bread_model_test.cs
@@ -0,0 +1,22 @@
+﻿namespace net6_bread_be_tests;
+using net6_bread_be.Models;
+
+public class BreadTests
+{
+    [Fact]
+    public void BreadProperties()
+    {
+        Bread bread = new Bread
+        {
+            BreadId = 1,
+            Name = "Hello",
+            Recipe = "I hate autocorrect",
+            Description = "World"
+        };
+
+        Assert.Equal(1, bread.BreadId);
+        Assert.Equal("Hello", bread.Name);
+        Assert.Equal("I hate autocorrect", bread.Recipe);
+        Assert.Equal("world", bread.Description);
+    }
+}
