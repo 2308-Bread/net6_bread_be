b6aee335f4901e01e0c5493881a412f7d09d011d
Eric Belongea <eric.belongea@gmail.com>
2024-02-26 14:14:18 -0700

    Country Validations
    Creates country validations along with happy and sad path testing. Country Model.

--- net6_bread_be/Models/Country.cs
+++ net6_bread_be/Models/Country.cs
@@ -6,7 +6,7 @@ namespace net6_bread_be.Models
 	public class Country
 	{
 		public int CountryId { get; set; }
-		[Required(ErrorMessage = "Name is require")]
+		[Required(ErrorMessage = "Name is required")]
 		public string Name { get; set; } = "";
 		[Required(ErrorMessage = "Description is required")]
 		public string Description { get; set; } = "";
--- net6_bread_be_tests/Models/country_test.cs
+++ net6_bread_be_tests/Models/country_test.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 namespace net6_bread_be_tests.Models;
+using System.ComponentModel.DataAnnotations;
+
+using System.Threading;
 using net6_bread_be.Models;
 
 public class CountryTests
@@ -10,13 +13,60 @@ public class CountryTests
 		Country country = new Country
 		{
 			CountryId = 1,
-			Name = "Scotland",
-			Description = "Braveheart"
-		};
+            Name = "Scotland",
+            Description = "Braveheart"
+        };
 
-		Assert.Equal(1, country.CountryId);
+        var validationContext = new ValidationContext(country, serviceProvider: null, items: null);
+        var validationResults = new List<ValidationResult>();
+        bool isValid = Validator.TryValidateObject(country, validationContext, validationResults, validateAllProperties: true);
+
+        Assert.Equal(1, country.CountryId);
 		Assert.Equal("Scotland", country.Name);
 		Assert.Equal("Braveheart", country.Description);
+
+		Assert.True(isValid);
+		Assert.Empty(validationResults);
 	}
+
+	//Sad Path testing
+
+	[Fact]
+	public void CountrySadPathName()
+	{
+		Country country = new Country
+		{
+			CountryId = 1,
+			Name = "",
+			Description = "Words"
+		};
+
+        var validationContext = new ValidationContext(country, serviceProvider: null, items: null);
+        var validationResults = new List<ValidationResult>();
+        bool isValid = Validator.TryValidateObject(country, validationContext, validationResults, validateAllProperties: true);
+
+        Assert.False(isValid);
+        Assert.Single(validationResults);
+        Assert.Equal("Name is required", validationResults[0].ErrorMessage);
+    }
+
+    [Fact]
+    public void CountrySadPathDescription()
+    {
+        Country country = new Country
+        {
+            CountryId = 1,
+            Name = "Land",
+            Description = ""
+        };
+
+        var validationContext = new ValidationContext(country, serviceProvider: null, items: null);
+        var validationResults = new List<ValidationResult>();
+        bool isValid = Validator.TryValidateObject(country, validationContext, validationResults, validateAllProperties: true);
+
+        Assert.False(isValid);
+        Assert.Single(validationResults);
+        Assert.Equal("Description is required", validationResults[0].ErrorMessage);
+    }
 }
 
