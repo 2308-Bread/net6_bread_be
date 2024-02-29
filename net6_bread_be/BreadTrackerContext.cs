b0954ca45765effa8033e94a89ffccc4caf58a3b
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-02-28 21:21:42 -0700

    Initial migration has been made, connected to postgres

--- net6_bread_be/BreadTrackerContext.cs
+++ net6_bread_be/BreadTrackerContext.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using net6_bread_be;
+using net6_bread_be.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace net6_bread_be
 {
-	public class BreadTrackerContext
+	public class BreadTrackerContext : DbContext
 	{
-		public BreadTrackerContext()
+		public DbSet<Bread>? Breads { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
 		{
+			optionsBuilder.UseNpgsql("Host=localhost;Username=postgres;Password=password123;Database=breadDb").UseSnakeCaseNamingConvention();
 		}
 	}
 }
